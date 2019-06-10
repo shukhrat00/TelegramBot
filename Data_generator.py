ca70513d8b9365c9e761de47e046f67542b92938
VictorFanta <51254047+VictorFanta@users.noreply.github.com>
2019-06-10 22:02:17 +0300

    0.8
    Finished flight generator

--- Data_generator.py
+++ Data_generator.py
@@ -2,21 +2,32 @@ import json
 import random
 import datetime
 
-class sheduleChunk():
+class scheduleChunk():
     def __init__(self):
         with open("Data_generator_constants.txt", 'r') as file:
-            data = json.load(file)
-        dates = ()
+            self.data = json.load(file)
+        self.schedules = {}
+        self.filledDates = set()
 
-        def generate_schedule(self):
-            schedules = {}
-            for airport in data["Airports"]:
-                schedules[airport[0]] = {}
-                for terminal in airport[1]:
-                    schedules[airport[0]][terminal] = {}
-                    minutes = 0
-                    while minutes < 24 * 60:
-                        minutes += random.randint(0, 100)
-                        airline = airport[2][random.randint(0, len(airport[2]) - 1)]
-                        schedules[airport[0]][terminal][minutes] = (data['Flight_samples'][airport[0]][airline][random.randint(0, len(data['Flight_samples'][airport[0]][airline]) - 1)], airline)
-                    
+    def generate_schedule(self):
+        schedule = {}
+        for airport in self.data["Airports"]:
+            schedule[airport[0]] = {}
+            for terminal in airport[1]:
+                schedule[airport[0]][terminal] = {}
+                minutes = 0
+                while minutes < 24 * 60:
+                    minutes += random.randint(0, 100)
+                    airline = airport[2][random.randint(0, len(airport[2]) - 1)]
+                    schedule[airport[0]][terminal][minutes] = (self.data['Flight_samples'][airport[0]][airline][random.randint(0, len(self.data['Flight_samples'][airport[0]][airline]) - 1)], airline)
+        return schedule
+    
+    def check_date(self, date):
+        if date in self.filledDates:
+            return True
+        else:
+            return False
+            
+    def fill_date(self, date):
+        self.filledDates.add(date)
+        self.schedules[date] = self.generate_schedule()
